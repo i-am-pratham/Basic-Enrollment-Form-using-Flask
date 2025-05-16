2d679e3b64032189f98733e767b77768ddd8fd47
i-am-pratham <sarodeprathamesh26@gmail.com>
2025-05-16 10:50:18 +0530

    add routes.py and update .gitignore

--- application/__init__.py
+++ application/__init__.py
@@ -2,10 +2,8 @@
 from flask import Flask
 
 app = Flask(__name__)
-@app.route('/')
-@app.route('/index')
-def index():
-    return "<h1>Hello, World!!!</h1>"
+
+from application import routes
 
 if __name__ == '__main__':
     app.run(debug=True)
