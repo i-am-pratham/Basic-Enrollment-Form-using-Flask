c54d4eff5e3bad2466c458fe45277e20b7028f33
i-am-pratham <sarodeprathamesh26@gmail.com>
2025-05-22 20:51:35 +0530

    add database instance , solve little footer  bug

--- application/__init__.py
+++ application/__init__.py
@@ -1,7 +1,14 @@
-
 from flask import Flask
+from config import Config
+from flask_mongoengine import MongoEngine
 
+# Initialize Flask application
+# and MongoDB connection
 app = Flask(__name__)
+
+app.config.from_object(Config)
+db  = MongoEngine()
+db.init_app(app)
 
 from application import routes
 
